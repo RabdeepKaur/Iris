61d8714db16b3fc4083eb722ccf6c650be0e39cc
RabddepKaur <rabdeep0202@gmail.com>
2025-04-02 16:19:25 +0530

    tailwind

--- client/client/src/screens/home.jsx
+++ client/client/src/screens/home.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import "./ui/home.css";
+import Navbar from '../components/Navbar';
 
 const Homepage= ()=>{
     return(
         <>
-        <div className='navbar'>
-           <nav className='navbar'>
-           <a className="nav-item active" href="#">Home</a>
-           <a className="nav-item" href="#">Lobby</a>
-           <a className="nav-item" href="#">About</a>
-      
-           </nav>
+        <div>
+        <Navbar/>
         </div>
         </>
     )
